ecae7bf9d649be5c9cd90c9ca5a9ed4e10d76f80
Diego Aarón <diegoaarondv@gmail.com>
2022-08-18 13:31:16 -0500

    recuperando informacion con where

--- 06_recuperar_informacion_where.sql
+++ 06_recuperar_informacion_where.sql
@@ -49,3 +49,26 @@ select nombre from agenda where telefono = '4545454';
 
 -- Ejercicio 2 
 
+drop table articulos;
+
+create table articulos(
+    codigo number(5),
+    nombre varchar2(20),
+    descripcion varchar2(30),
+    precio number(7,2)
+);
+
+describe articulos;
+
+insert into articulos (codigo, nombre, descripcion, precio) values (1,'impresora','Epson Stylus C45',400.80);
+insert into articulos (codigo, nombre, descripcion, precio) values (2,'impresora','Epson Stylus C85',500);
+insert into articulos (codigo, nombre, descripcion, precio) values (3,'monitor','Samsung 14',800);
+insert into articulos (codigo, nombre, descripcion, precio) values (4,'teclado','ingles Biswal',100);
+insert into articulos (codigo, nombre, descripcion, precio) values (5,'teclado','español Biswal',90);
+
+commit;
+
+select * from articulos where nombre = 'impresora';
+
+select codigo, descripcion, precio from articulos where nombre = 'teclado';
+
