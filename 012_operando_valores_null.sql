caa01e838b390ee2fb5c2458361fac69ad03833e
Diego Aarón <diegoaarondv@gmail.com>
2022-08-21 11:01:31 -0500

    operando valores nulos

--- 012_operando_valores_null.sql
+++ 012_operando_valores_null.sql
@@ -83,3 +83,40 @@ select * from medicamentos where laboratorio is not null;
 
 drop table peliculas;
 
+create table peliculas(
+  codigo number(4) not null,
+  titulo varchar2(40) not null,
+  actor varchar2(20),
+  duracion number(3)
+);
+
+describe peliculas;
+
+insert into peliculas (codigo,titulo,actor,duracion) values(1,'Mision imposible','Tom Cruise',120);
+insert into peliculas (codigo,titulo,actor,duracion) values(2,'Harry Potter y la piedra filosofal',null,180);
+insert into peliculas (codigo,titulo,actor,duracion) values(3,'Harry Potter y la camara secreta','Daniel R.',null);
+insert into peliculas (codigo,titulo,actor,duracion) values(0,'Mision imposible 2','',150);
+insert into peliculas (codigo,titulo,actor,duracion) values(4,'Titanic','L. Di Caprio',220);
+insert into peliculas (codigo,titulo,actor,duracion) values(5,'Mujer bonita','R. Gere-J. Roberts',0);
+
+select * from peliculas;
+
+insert into peliculas(codigo, titulo, actor, duracion) values(6,'','ASD',15); -- error
+
+select * from peliculas where actor is null;
+
+update peliculas set duracion = 120 where duracion is null;
+
+select * from peliculas where duracion = 120;
+
+update peliculas set actor='Desconocido' where actor is null;
+
+select * from peliculas where actor is null;
+
+update peliculas set duracion = null where duracion = 0;
+
+delete from peliculas where duracion is null;
+
+commit;
+
+
