9df62ac75c6055141bc0b77833e235fa8f32adec
Diego Aarón <diegoaarondv@gmail.com>
2022-08-27 19:53:29 -0500

    probando funciones para string

--- 021_funciones_string.sql
+++ 021_funciones_string.sql
@@ -79,3 +79,29 @@ select replace('xxx.oracle.com','x','w') from dual;
 -- empezando desde la posición especificada por el segundo argumento y de tantos caracteres de longitud como 
 -- indica el tercer argumento.
 
+select substr('www.oracle.com',1,10) from dual;
+
+select substr('www.oracle.com',5,6) from dual;
+
+-- length(cadena): retorna la longitud de la cadena enviada como argumento. "lenght" significa longitud en inglés.
+
+select length('www.oracle.com') from dual;
+
+-- instr (cadena,subcadena): devuelve la posición de comienzo (de la primera ocurrencia) de la subcadena especificada 
+-- en la cadena enviada como primer argumento. Si no la encuentra retorna 0.
+
+select instr('Jorge Luis Borges','or') from dual;
+
+select instr('Jorge Luis Borges','ar') from dual;
+
+-- translate(): reemplaza cada ocurrencia de una serie de caracteres con otra serie de caracteres. La diferencia 
+-- con "replace" es que aquella trabaja con cadenas de caracteres y reemplaza una cadena completa por otra, 
+-- en cambio "translate" trabaja con caracteres simples y reemplaza varios. En el siguiente ejemplo se especifica que se 
+-- reemplacen todos los caracteres "O" por el caracter "0", todos los caracteres "S" por el caracter "5" y todos los 
+-- caracteres "G" por "6"
+
+select translate('JORGE LUIS BORGES','OSG','056') from dual;
+
+-- Se pueden emplear estas funciones enviando como argumento el nombre de un campo de tipo caracter.
+
+-- Ejercicio 1
