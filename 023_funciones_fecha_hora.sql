123bc5993f47f2b11072410295e01384d0ba7d09
Diego Aarón <diegoaarondv@gmail.com>
2022-08-28 21:27:20 -0500

    practica de funciones de fecha y hora

--- 023_funciones_fecha_hora.sql
+++ 023_funciones_fecha_hora.sql
@@ -61,3 +61,51 @@ select sysdate - 3 from dual;
 
 select to_date('15/12/2020') - 5 from dual;
 
+-- Ejercicio 1
+
+drop table libros;
+
+create table libros(
+  titulo varchar2(40) not null,
+  autor varchar2(20) default 'Desconocido',
+  editorial varchar2(20),
+  edicion date,
+  precio number(6,2)
+);
+
+insert into libros values('El aleph','Borges','Emece','10/10/1980',25.33);
+insert into libros values('Java en 10 minutos','Mario Molina','Siglo XXI','05/05/2000',50.65);
+insert into libros values('Alicia en el pais de las maravillas','Lewis Carroll','Emece','08/09/2000',19.95);
+insert into libros values('Aprenda PHP','Mario Molina','Siglo XXI','02/04/2000',45);
+
+-- mostrando el título y el año de edición
+
+select titulo, extract (year from edicion) from libros;
+
+-- mostrando el titulo del libro y el mes de edicion
+
+select titulo, extract(month from edicion) from libros;
+
+-- mostrando el titulo del libro y los años que tienen de editados:
+
+select titulo, extract(year from sysdate) - extract(year from edicion) from libros;
+
+-- mostrando los titulos de los libros que se editaron en el año 2000
+
+select titulo from libros where extract(year from edicion) = 2000;
+
+-- calcule 3 meses luego de la fecha actual empleando "add_months"
+
+select add_months(sysdate, 3) from dual;
+
+-- muestre la fecha del primer martes desde la fecha actual
+
+select next_day(sysdate, 'MARTES') from dual;
+
+-- muestra la fecha que sera 15 días después de "24/08/2018" empleando el operador "+"
+
+select to_date('24/08/2018') + 15 from dual;
+
+-- muestre la fecha que 20 días antes del "12/08/2018" empleando el operador "-"
+
+select to_date('12/08/2018') - 20 from dual;
