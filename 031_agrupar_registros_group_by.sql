2432f46792ef410584ef7f1a40fde6d356007d96
Diego Aarón <diegoaarondv@gmail.com>
2022-09-01 20:34:41 -0500

    agrupar registros con GROUP BY

--- 031_agrupar_registros_group_by.sql
+++ 031_agrupar_registros_group_by.sql
@@ -96,4 +96,45 @@ select ciudad, avg(montocompra) from visitantes group by ciudad;
 
 select ciudad, count(*) from visitantes where mail is not null and mail <> 'no tiene'  group by ciudad;
 
+-- Ejercicio 2 
+
+drop table empleados;
+
+create table empleados(
+  nombre varchar2(30),
+  documento char(8),
+  domicilio varchar2(30),
+  seccion varchar2(20),
+  sueldo number(6,2),
+  cantidadhijos number(2),
+  fechaingreso date,
+  primary key(documento)
+ );
+
+insert into empleados values('Juan Perez','22333444','Colon 123','Gerencia',5000,2,'10/05/1980');
+insert into empleados values('Ana Acosta','23444555','Caseros 987','Secretaria',2000,0,'12/10/1980');
+insert into empleados values('Lucas Duarte','25666777','Sucre 235','Sistemas',4000,1,'25/05/1985');
+insert into empleados values('Pamela Gonzalez','26777888','Sarmiento 873','Secretaria',2200,3,'25/06/1990');
+insert into empleados values('Marcos Juarez','30000111','Rivadavia 801','Contaduria',3000,0,'01/05/1996');
+insert into empleados values('Yolanda Perez','35111222','Colon 180','Administracion',3200,1,'01/05/1996');
+insert into empleados values('Rodolfo Perez','35555888','Coronel Olmedo 588','Sistemas',4000,3,'01/05/1996');
+insert into empleados values('Martina Rodriguez','30141414','Sarmiento 1234','Administracion',3800,4,'01/09/2000');
+insert into empleados values('Andres Costa','28444555',default,'Secretaria',null,null,null);
+
+-- Cuente la cantidad de empleados agrupados por sección
+
+select seccion, count(*) from empleados group by seccion;
+
+-- Calcule el promedio de hijos por sección
+
+select seccion, avg(cantidadhijos) from empleados group by seccion;
+
+-- Cuente la cantidad de empleados agrupados por año de ingreso
+
+select extract(year from fechaingreso) as ingreso, count(*) from empleados group by extract(year from fechaingreso);
+
+-- Calcule el promedio de sueldo por sección de los empleados con hijos
+
+select seccion, avg(sueldo) from empleados where cantidadhijos > 0 group by seccion;
+
 commit;
