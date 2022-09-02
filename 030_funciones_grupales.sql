663d6b35bfc3d0c557d08f5d620e04a34239e190
Diego Aarón <diegoaarondv@gmail.com>
2022-09-01 19:42:05 -0500

    evaluando funciones avg-max-min

--- 030_funciones_grupales.sql
+++ 030_funciones_grupales.sql
@@ -62,5 +62,84 @@ select min(edicion) from libros;
 
 select max(codigo) from libros;
 
+-- Ejercicio 1
 
+drop table empleados;
 
+create table empleados(
+  nombre varchar2(30),
+  documento char(8),
+  domicilio varchar2(30),
+  seccion varchar2(20),
+  sueldo number(6,2),
+  cantidadhijos number(2),
+  fechaingreso date,
+  primary key(documento)
+);
+
+insert into empleados values('Juan Perez','22333444','Colon 123','Gerencia',5000,2,'10/10/1980');
+insert into empleados values('Ana Acosta','23444555','Caseros 987','Secretaria',2000,0,'15/08/1998');
+insert into empleados values('Lucas Duarte','25666777','Sucre 235','Sistemas',4000,1,null);
+insert into empleados values('Pamela Gonzalez','26777888','Sarmiento 873','Secretaria',2200,3,null);
+insert into empleados values('Marcos Juarez','30000111','Rivadavia 801','Contaduria',3000,0,'26/08/2000');
+insert into empleados values('Yolanda Perez','35111222','Colon 180','Administracion',3200,1,'25/09/2001');
+insert into empleados values('Rodolfo Perez','35555888','Coronel Olmedo 588','Sistemas',4000,3,null);
+insert into empleados values('Martina Rodriguez','30141414','Sarmiento 1234','Administracion',3800,4,'14/12/2000');
+insert into empleados values('Andres Costa','28444555',default,'Secretaria',null,null,'08/08/1990');
+
+-- Muestre la cantidad de empleados usando "count"
+
+select count(nombre) from empleados;
+
+-- Muestre la cantidad de empleados con fecha de ingreso conocida
+
+select count(fechaingreso) from empleados;
+
+-- Muestre la cantidad de empleados con sueldo
+
+select count(sueldo) from empleados;
+
+-- Muestre la cantidad de empleados con sueldo de la sección "Secretaria"
+
+select count(sueldo) from empleados where seccion = 'Secretaria';
+
+-- Muestre el sueldo más alto y el más bajo colocando un alias 
+
+select max(sueldo) as "maximo", min(sueldo) as "minimo" from empleados;
+
+-- Muestre el valor mayor de "cantidadhijos" de los empleados "Perez"
+
+select max(cantidadhijos) from empleados where nombre like '%Perez%';
+
+-- Muestre la fecha de ingreso más reciente (max) y la más lejana (min)
+
+select max(fechaingreso) as "mas reciente", min(fechaingreso) as "mas lejana" from empleados;
+
+-- Muestre el documento menor
+
+select min(documento) from empleados;
+
+-- Muestre el promedio de sueldos de todo los empleados
+
+select avg(sueldo) from empleados;
+
+-- Muestre el promedio de sueldos de los empleados de la sección "Secretaría"
+
+select avg(sueldo) from empleados where seccion = 'Secretaria';
+
+-- Muestre el promedio de hijos de todos los empleados de "Sistemas"
+
+select avg(cantidadhijos) from empleados where seccion = 'Sistemas';
+
+-- Muestre la cantidad de empleados, la cantidad de empleados con domicilio conocido, la suma de los hijos, 
+-- el promedio de los sueldos y los valores mínimo y máximo del campo "fechaingreso" de todos los empleados.
+
+select count(nombre) as "cant. empleados", count(domicilio) as "emp. domic. conocido", sum(cantidadhijos), 
+avg(sueldo), min(fechaingreso), max(fechaingreso) from empleados;
+
+-- Realice la misma consulta anterior pero ahora de los empleados de la sección "Recursos".
+
+select count(nombre) as "cant. empleados", count(domicilio) as "emp. domic. conocido", sum(cantidadhijos), 
+avg(sueldo), min(fechaingreso), max(fechaingreso) from empleados where seccion = 'Recursos';
+
+commit;
