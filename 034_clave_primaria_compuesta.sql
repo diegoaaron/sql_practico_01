cc21af0585abb889f0755b95b92c80ef9bfb53b5
Diego Aarón <diegoaarondv@gmail.com>
2022-09-04 17:59:39 -0500

    definiendo clave primaria compuesta

--- 034_clave_primaria_compuesta.sql
+++ 034_clave_primaria_compuesta.sql
@@ -35,14 +35,52 @@ on ucc.constraint_name=uc.constraint_name
 where uc.constraint_type='P' and
 uc.table_name='VEHICULOS';
 
+-- Una playa de estacionamiento almacena cada día los datos de los vehículos 
+-- que ingresan en la tabla llamada "vehiculos".
 
+-- Seteamos el formato de "date" para que nos muestre únicamente la hora y los minutos, 
+-- ya que en esta playa, se almacenan los datos de los vehículos diariamente:
 
+ALTER SESSION SET NLS_DATE_FORMAT = 'HH24:MI';
 
+drop table vehiculos;
 
+create table vehiculos(
+  patente char(6) not null,
+  tipo char(1),--'a'=auto, 'm'=moto
+  horallegada date,
+  horasalida date,
+  primary key(patente,horallegada)
+);
 
+insert into vehiculos values('AIC124','a','8:05','12:30');
+insert into vehiculos values('CAA258','a','8:05',null);
+insert into vehiculos values('DSE367','m','8:30','18:00');
+insert into vehiculos values('FGT458','a','9:00',null);
+insert into vehiculos values('AIC124','a','16:00',null);
+insert into vehiculos values('LOI587','m','18:05','19:55');
 
+-- Si intentamos ingresar un registro con clave primaria repetida
 
+insert into vehiculos values('LOI587','m','18:05',null);
 
+-- Si uno de los campos es repetidos si lo acepta, ya que la llave primaria es compuesta
+
+insert into vehiculos values('LOI587','m','21:30',null);
+
+select * from vehiculos;
+
+-- Si intentamos actualizar un registro repitiendo la clave primaria, mostrar un error
+
+update vehiculos set horallegada = '08:05' where patente='AIC124' and horallegada='16:00';
+
+-- la llaves primarias no aceptan null aunque no se definiera en la creación de la tabla
+
+insert into vehiculos values('HUO690','m',null,null);
+
+describe vehiculos;
+
+-- Ejercicio 1
 
 
 
