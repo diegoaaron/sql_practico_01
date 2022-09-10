3f589b6939350d5c6894b4e7c3b1cfd219536630
Diego Aarón <diegoaarondv@gmail.com>
2022-09-10 15:39:25 -0500

    eliminando restricciones

---
+++ 043_restricciones_eliminacion.sql
@@ -0,0 +1,53 @@
+/*
+
+Para eliminar una restricción, la sintaxis básica es la siguiente:
+
+ alter table NOMBRETABLA
+  drop constraint NOMBRERESTRICCION;
+  
+Cuando eliminamos una tabla, todas las restricciones que fueron establecidas en ella, se eliminan también.
+
+La condición de control que debe cumplir una restricción de control no puede modificarse, 
+hay que eliminar la restricción y volver a crearla; igualmente con las restricciones "primary key" y "unique", 
+no pueden modificarse los campos.
+
+*/
+
+drop table libros;
+
+create table libros(
+  codigo number(5) not null,
+  titulo varchar2(40),
+  autor varchar2(30),
+  editorial varchar2(15),
+  precio number(6,2)
+);
+
+-- Definimos una restricción "primary key" para nuestra tabla "libros" para asegurarnos que cada libro tendrá 
+-- un código diferente y único:
+
+alter table libros
+add constraint PK_LIBROS_CODIGO
+primary key(codigo);
+
+-- Definimos una restricción "check" para asegurarnos que el precio no será negativo:
+
+alter table libros
+add constraint CK_LIBROS_PRECIO
+check (precio > 0);
+
+-- Definimos una restricción "unique" para los campos "titulo", "autor" y "editorial":
+
+alter table libros
+add constraint UQ_LIBROS
+unique (titulo, autor, editorial);
+
+-- Vemos las restricciones:
+
+select * from user_constraints where table_name = 'LIBROS';
+
+
+
+
+
+
