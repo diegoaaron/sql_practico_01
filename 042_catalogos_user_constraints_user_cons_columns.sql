96034e08d45f0e79d18e53124af8d6ac184ddaa3
Diego Aarón <diegoaarondv@gmail.com>
2022-09-10 15:20:17 -0500

    analizando las restricciones con user_constraints y user_cons_columns

---
+++ 042_catalogos_user_constraints_user_cons_columns.sql
@@ -0,0 +1,38 @@
+/*
+El catálogo "user_constraints" muestra la información referente a todas las restricciones establecidas en las tablas 
+del usuario actual, devuelve varias columnas, explicaremos algunas de ellas:
+
+- owner: propietario de la restricción
+
+- constraints_name: el nombre de la restricción
+
+- constraint_type: tipo de restricción. Si es una restricción de control muestra el caracter "C", 
+si es "primary key" muestra "P", si es "unique" el caracter "U".
+
+- table_name: nombre de la tabla en la cual se estableció la restricción
+
+- search_condition: solamente es aplicable a restricciones de control; indica la condición de chequeo a cumplirse.
+
+- status: indica si está habilitada (enabled) para futuras inserciones y actualizaciones o deshabilitada (disabled)
+
+- validated: indica si valida los datos existentes en la tabla (validated) o no (no validate)
+
+El catálogo "user_cons_columns" muestra la información referente a todas las restricciones establecidas en 
+las tablas del usuario actual, devuelve las siguientes columnas:
+
+- owner: propietario de la restricción;
+
+- constraints_name: el nombre de la restricción;
+
+- table_name: nombre de la tabla en la cual se estableció;
+
+- column_name: muestra cada campo en el cual la restricción fue aplicada.
+
+- position: solamente es aplicable a restricciones "primary key" y "unique"; indica el orden en que fueron definidos 
+los campos que componen la clave (primaria o única).
+
+*/
+
+select * from user_constraints;
+
+select * from user_cons_columns;
