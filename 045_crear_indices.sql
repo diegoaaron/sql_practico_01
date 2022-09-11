6634e90abdbce46a6a7937b2e15b4f429f3bf4e2
Diego Aarón <diegoaarondv@gmail.com>
2022-09-10 19:37:36 -0500

    manipulando INDICES

--- 045_crear_indices.sql
+++ 045_crear_indices.sql
@@ -62,4 +62,109 @@ Para obtener información sobre los índices podemos consultar varios diccionarios
 4) "user_constraints": si la restricción tiene un índice asociado, aparece su nombre en la columna "INDEX_NAME".
 */
 
+drop table empleados;
+
+create table empleados(
+  legajo number(5),
+  documento char(8),
+  apellido varchar2(25),
+  nombre varchar2(25),
+  domicilio varchar2(30)
+);
+
+-- Agregamos una restricción "primary key" sobre el campo "legajo":
+
+alter table empleados
+add constraint PK_EMPLEADOS_LEGAJO
+primary key(legajo);
+
+-- Consultamos "user_constraints":
+
+select constraint_name, constraint_type, index_name from user_constraints
+where table_name = 'EMPLEADOS';
+
+-- Note que Oracle creó un índice con el mismo nombre de la restricción.
+
+-- Veamos los índices de "empleados":
+
+select index_name, index_type, uniqueness from user_indexes
+where table_name = 'EMPLEADOS';
+
+--  Aparece 1 fila, mostrando el nombre del índice, indicando que es normal y único.
+
+-- Creamos un índice único sobre el campo "documento":
+
+create unique index I_EMPLEADOS_DOCUMENTO
+on empleados(documento);
+
+-- Verificamos que se creó el índice
+
+select index_name, index_type, uniqueness from user_indexes
+where table_name = 'EMPLEADOS';
+
+-- Aparecen 2 filas, una por cada índice.
+
+-- Agregamos a la tabla una restricción única sobre el campo "documento":
+
+alter table empleados
+add constraint UQ_EMPLEADOS_DOCUMENTO
+unique (documento);
+
+-- Analicemos la información que nos muestra "user_constraints"
+
+select constraint_name, constraint_type, index_name
+from user_constraints
+where table_name = 'EMPLEADOS';
+
+-- En la columna "index_name" correspondiente a la restricción única, aparece "I_EMPLEADOS_DOCUMENTO", 
+-- Oracle usa para esta restricción el índice existente, no crea otro nuevo.
+
+-- Creamos un índice no único, compuesto (para los campos "apellido" y "nombre")
+
+create index I_EMPLEADOS_APELLIDONOMBRE
+on empleados (apellido, nombre);
+
+-- Consultamos el diccionario "user_indexes". Nos muestra información sobre los 3 índices de la tabla.
+
+select index_name, index_type, uniqueness from user_indexes
+where table_name = 'EMPLEADOS';
+
+-- Veamos todos los índices de la base de datos activa consultando "user_objects". Aparecen varios índices, entre ellos, 
+-- los de nuestra tabla "empleados".
+
+select * from user_objects where object_type = 'INDEX';
+
+-- Obtenemos información de "user_ind_columns". La tabla tiene 3 índices, 2 filas corresponden al índice compuesto 
+-- "I_empleados_apellidonombre"; la columna "position" indica el orden de los campos indexados.
+
+select index_name, column_name, column_position from user_ind_columns
+where table_name = 'EMPLEADOS';
+
+-- Agregamos algunos registros
+
+insert into empleados values(1,'22333444','Lopez','Juan','Colon 123');
+insert into empleados values(2,'23444555','Lopez','Luis','Lugones 1234');
+insert into empleados values(3,'24555666','Garcia','Pedro','Avellaneda 987');
+insert into empleados values(4,'25666777','Garcia','Ana','Caseros 678');
+
+-- Si intentamos crear un índice único para el campo "apellido" (que contiene valores duplicados") Oracle no lo permite:
+
+create unique index I_EMPLEADOS_APELLIDO
+on empleados(apellido);
+
+-- Igualmente, si hay un índice único sobre un campo y luego intentamos ingresar un registro con un valor repetido para 
+-- el campo indexado, Oracle no lo permite.
+
+-- Creamos un índice único sobre el campo "nombre"
+
+create unique index I_EMPLEADOS_NOMBRE
+on empleados(nombre);
+
+-- Oracle lo permite porque no hay valores duplicados.
+
+-- Intentamos agregamos un registro que repita un nombre:
+
+insert into empleados values(5,'30111222','Perez','Juan','Bulnes 233');
+
+-- oracle no lo permite. 
 
