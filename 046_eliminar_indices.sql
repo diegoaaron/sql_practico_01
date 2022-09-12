0d6cd18d3c5c01407d181f4c78442695fe459323
Diego Aarón <diegoaarondv@gmail.com>
2022-09-11 23:36:55 -0500

    eliminar indices

--- 046_eliminar_indices.sql
+++ 046_eliminar_indices.sql
@@ -22,5 +22,33 @@ drop table empleados;
  create unique index I_EMPLEADOS_LEGAJO 
  on empleados(legajo);
  
+-- Agregamos una restricción "unique" sobre "legajo":
+
+alter table empleados
+add constraint UQ_EMPLEADOS_LEGAJO
+unique (legajo);
+
+-- Verificamos que la restricción usa el índice creado anteriormente, no crea otro
+
+select constraint_name, constraint_type, index_name
+from user_constraints
+where table_name='EMPLEADOS';
+
+-- Agregamos una restricción "primary key" sobre "documento"
+
+alter table empleados
+add constraint PK_EMPLEADOS_DOCUMENTO
+primary key(documento);
+ 
+ -- Verificamos que Oracle creó un índice para el campo "documento"
+ 
+select constraint_name, constraint_type, index_name
+from user_constraints
+where table_name='EMPLEADOS'; 
+ 
+--  Consultamos todos los índices y sus tipos consultando "user_indexes"
+
+
+
 
  
