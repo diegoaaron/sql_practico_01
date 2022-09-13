dd5198279ba80d97e05cf88b5d5a7a88962629c5
Diego Aarón <diegoaarondv@gmail.com>
2022-09-12 22:36:21 -0500

    INNER JOIN o JOIN

--- 048_join_combinacion_interna.sql
+++ 048_join_combinacion_interna.sql
@@ -33,8 +33,41 @@ Analicemos la consulta anterior.
 - indicamos el nombre de la tabla luego del "from" ("libros");
 
 - combinamos esa tabla con "join" y el nombre de la otra tabla ("editoriales"); se especifica qué tablas se van a 
-combinar y cómo;
+combinar y cómo
 
+- cuando se combina información de varias tablas, es necesario especificar qué registro de una tabla se combinará con qué registro de la otra tabla, con "on". Se debe especificar la condición para enlazarlas, es decir, el campo por el cual se combinarán, que tienen en común. "on" hace coincidir registros de ambas tablas basándose en el valor de tal campo, en el ejemplo, el campo "codigoeditorial" de "libros" y el campo "codigo" de "editoriales" son los que enlazarán ambas tablas. Se emplean campos comunes, que deben tener tipos de datos iguales o similares.
+
+La condicion de combinación, es decir, el o los campos por los que se van a combinar (parte "on"), 
+se especifica según las claves primarias y externas.
+
+Note que en la consulta, al nombrar el campo usamos el nombre de la tabla también. Cuando las tablas referenciadas 
+tienen campos con igual nombre, esto es necesario para evitar confusiones y ambiguedades al momento de referenciar
+un campo. En el ejemplo, si no especificamos "editoriales.codigo" y solamente tipeamos "codigo", Oracle no sabrá si 
+nos referimos al campo "codigo" de "libros" o de "editoriales" y mostrará un mensaje de error indicando que "codigo" 
+es ambiguo.
+
+Entonces, si las tablas que combinamos tienen nombres de campos iguales, DEBE especificarse a qué tabla pertenece 
+anteponiendo el nombre de la tabla al nombre del campo, separado por un punto (.).
+
+Si una de las tablas tiene clave primaria compuesta, al combinarla con la otra, en la cláusula "on" se debe
+hacer referencia a la clave completa, es decir, la condición referenciará a todos los campos clave que identifican al registro.
+
+Se puede incluir en la consulta join la cláusula "where" para restringir los registros que retorna el resultado; 
+también "order by", "distinct", etc..
+
+Se emplea este tipo de combinación para encontrar registros de la primera tabla que se correspondan con 
+los registros de la otra, es decir, que cumplan la condición del "on". Si un valor de la primera tabla no se encuentra
+en la segunda tabla, el registro no aparece; si en la primera tabla el valor es nulo, tampoco aparece.
+
+Para simplificar la sentencia podemos usar un alias para cada tabla:
+
+select l.codigo,titulo,autor,nombre
+from libros l
+join editoriales e
+on l.codigoeditorial=e.codigo;
+  
+En algunos casos (como en este ejemplo) el uso de alias es para fines de simplificación y hace más legible la consulta 
+si es larga y compleja, pero en algunas consultas es absolutamente necesario.
 */
 
 
