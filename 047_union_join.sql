a09442af632f184d83838e29999c5bdd494b0c74
Diego Aarón <diegoaarondv@gmail.com>
2022-09-12 21:47:14 -0500

    realizando uniones de tabla con JOIN

---
+++ 047_union_join.sql
@@ -0,0 +1,12 @@
+/*
+Hasta el momento hemos trabajado con una sola tabla, pero generalmente, se trabaja con más de una.
+
+Para evitar la repetición de datos y ocupar menos espacio, se separa la información en varias tablas. 
+Cada tabla almacena parte de la información que necesitamos registrar.
+
+Por ejemplo, los datos de nuestra tabla "libros" podrían separarse en 2 tablas, una llamada "libros" y otra "editoriales" 
+que guardará la información de las editoriales. En nuestra tabla "libros" haremos referencia a la editorial colocando un 
+código que la identifique. Veamos:
+
+*/
+
