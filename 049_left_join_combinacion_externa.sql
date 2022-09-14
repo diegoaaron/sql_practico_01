7688fee692434789bbcc93f717666375cecf337c
Diego Aarón <diegoaarondv@gmail.com>
2022-09-13 21:17:09 -0500

    evaluando combinacione externas con LEFT JOIN

--- 049_left_join_combinacion_externa.sql
+++ 049_left_join_combinacion_externa.sql
@@ -73,5 +73,85 @@ coincidencia en la tabla de la derecha:
   left join libros l
   on e.codigo=codigoeditorial
   where codigoeditorial is null;
-
 */
+
+drop table libros;
+drop table editoriales;
+
+create table libros(
+  codigo number(5),
+  titulo varchar2(40),
+  autor varchar2(30),
+  codigoeditorial number(3)
+);
+
+create table editoriales(
+  codigo number(3),
+  nombre varchar2(20),
+  direccion varchar2(40)
+);
+ 
+alter table editoriales
+add constraints UQ_editoriales_codigo
+unique (codigo);
+
+alter table libros
+add constraints UQ_libros_codigo
+unique (codigo);
+
+insert into editoriales values(1,'Planeta','Colon 120');
+insert into editoriales values(2,'Emece','Estrada 356');
+insert into editoriales values(3,'Siglo XXI','General Paz 700');
+insert into editoriales values(null,'Sudamericana','Copiapo 343');
+insert into editoriales values(null,'Norma','Bulnes 123');
+
+insert into libros values(100,'El aleph','Borges',1);
+insert into libros values(200,'Martin Fierro','Jose Hernandez',1);
+insert into libros values(300,'Aprenda PHP','Mario Molina',2);
+insert into libros values(400,'Java en 10 minutos',default,4);
+insert into libros values(500,'El quijote de la mancha','Cervantes',null);
+
+-- realizamos una combinación izquierda para obtener los títulos de los libros, incluyendo el nombre de la editorial:
+-- Las editoriales de las cuales no hay libros, es decir, cuyo código de editorial no está presente en "libros" o tienen valor nulo, 
+-- aparece en el resultado, pero con el valor "null" en el campo "titulo" (caso de "Siglo XXI", "Sudamericana" y "Norma").
+
+select titulo, nombre
+from editoriales e
+left join libros l
+on e.codigo = codigoeditorial;
+
+-- Realizamos la misma consulta anterior pero cambiamos el orden de las tablas:
+-- El resultado mostrará el título del libro y el nombre de la editorial; los títulos cuyo código de editorial no está presente en 
+-- "editoriales" o tienen valor nulo, aparecen en el resultado, pero con el valor "null" en el campo "nombre" (caso de los libros 
+-- "El quijote..." y "Java...").
+
+select titulo, nombre
+from libros l
+left join editoriales e
+on codigoeditorial = e.codigo;
+
+-- Restringimos el resultado de una consulta considerando solamente los registros que encuentran coincidencia en la tabla 
+-- de la derecha, es decir, cuyo valor de código está presente en "libros":
+
+select titulo, nombre
+from editoriales e
+left join libros l
+on e.codigo = codigoeditorial
+where codigoeditorial is not null;
+
+-- Mostramos las editoriales que NO están presentes en "libros", es decir, que NO encuentran coincidencia en la 
+-- tabla de la derecha:
+
+select nombre
+from editoriales e
+left join libros l
+on e.codigo = codigoeditorial
+where codigoeditorial is null;
+
+-- Ejercicio 1 
+
+
+
+
+
+
