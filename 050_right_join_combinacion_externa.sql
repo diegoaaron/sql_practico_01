26689666c4a96699e5af8a09fd51ccf07ecd440d
Diego Aarón <diegoaarondv@gmail.com>
2022-09-13 23:17:11 -0500

    evaluando combinacione externas con RIGHT JOIN

--- 050_right_join_combinacion_externa.sql
+++ 050_right_join_combinacion_externa.sql
@@ -121,6 +121,67 @@ right join editoriales e
 on l.codigoeditorial = e.codigo
 where l.codigoeditorial is null;
 
+-- Ejercicio 1
 
+ drop table clientes;
+ drop table provincias;
 
+ create table clientes (
+  codigo number(5),
+  nombre varchar2(30),
+  domicilio varchar2(30),
+  ciudad varchar2(20),
+  codigoprovincia number(2),
+  primary key(codigo)
+ );
+
+ create table provincias(
+  codigo number(2),
+  nombre varchar2(20),
+  primary key (codigo)
+ );
+
+ insert into provincias values(1,'Cordoba');
+ insert into provincias values(2,'Santa Fe');
+ insert into provincias values(3,'Corrientes');
+
+ insert into clientes values (101,'Lopez Marcos','Colon 111','Córdoba',1);
+ insert into clientes values (102,'Perez Ana','San Martin 222','Cruz del Eje',1);
+ insert into clientes values (103,'Garcia Juan','Rivadavia 333','Villa Maria',1);
+ insert into clientes values (104,'Perez Luis','Sarmiento 444','Rosario',2);
+ insert into clientes values (105,'Gomez Ines','San Martin 666','Santa Fe',2);
+ insert into clientes values (106,'Torres Fabiola','Alem 777','La Plata',4);
+ insert into clientes values (107,'Garcia Luis','Sucre 475','Santa Rosa',5);
+
+-- Muestre todos los datos de los clientes, incluido el nombre de la provincia empleando un "right join".
+-- la segunda tabla (clientes) busca coincidencias en la primera (provincias) 
+
+select c.codigo, c.nombre, domicilio, ciudad, p.nombre as provincia
+from provincias p 
+right join clientes c
+on p.codigo = c.codigoprovincia;
+
+-- Obtenga la misma salida que la consulta anterior pero empleando un "left join".
+
+select c.codigo, c.nombre, domicilio, ciudad, p.nombre as provincia
+from clientes c 
+left join provincias p 
+on  c.codigoprovincia = p.codigo;
+
+-- Empleando un "right join", muestre solamente los clientes de las provincias que existen en "provincias" (5 registros)
+
+select c.codigo, c.nombre, domicilio, ciudad, p.nombre as provincia
+from provincias p 
+right join clientes c
+on  p.codigo = c.codigoprovincia 
+where p.codigo is not null;
+
+-- Muestre todos los clientes cuyo código de provincia NO existe en "provincias" ordenados por ciudad (2 registros)
+
+select c.codigo, c.nombre, domicilio, ciudad, p.nombre as provincia
+from provincias p 
+right join clientes c
+on  p.codigo = c.codigoprovincia 
+where p.codigo is null
+order by ciudad;
 
