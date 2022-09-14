b2ef3d13d0512b9314248969f117cc984b129cb4
Diego Aarón <diegoaarondv@gmail.com>
2022-09-13 22:49:38 -0500

    evaluando combinacione externas con RIGHT JOIN

--- 050_right_join_combinacion_externa.sql
+++ 050_right_join_combinacion_externa.sql
@@ -59,7 +59,67 @@ where codigoeditorial is null;
 
 */
 
+drop table libros;
+drop table editoriales;
 
+create table libros(
+  codigo number(5),
+  titulo varchar2(40),
+  autor varchar2(30),
+  codigoeditorial number(3)
+);
+
+create table editoriales(
+  codigo number(3),
+  nombre varchar2(20)
+);
+
+alter table libros
+add constraint PK_libros
+primary key(codigo);
+
+alter table editoriales
+add constraint PK_editoriales
+primary key(codigo);
+
+insert into editoriales values(1,'Planeta');
+insert into editoriales values(2,'Emece');
+insert into editoriales values(3,'Siglo XXI');
+insert into editoriales values(4,'Norma');
+
+insert into libros values(100,'El aleph','Borges',1);
+insert into libros values(101,'Martin Fierro','Jose Hernandez',1);
+insert into libros values(102,'Aprenda PHP','Mario Molina',2);
+insert into libros values(103,'Java en 10 minutos',null,4);
+insert into libros values(104,'El anillo del hechicero','Carol Gaskin',4);
+
+-- Solicitamos el título y nombre de la editorial de los libros empleando un "right join"
+-- Las editoriales de las cuales no hay libros, es decir, cuyo código de editorial no está presente en "libros" aparece 
+-- en el resultado, pero con el valor "null" en el campo "titulo"; caso de la editorial "Siglo XXI".
+
+select titulo, nombre as editorial
+from libros l
+right join editoriales e
+on l.codigoeditorial = e.codigo;
+
+-- Realizamos la misma consulta anterior agregando un "where" que restringa el resultado considerando solamente los 
+-- registros que encuentran coincidencia en la tabla izquierda:
+-- Ya no aparece la editorial "Siglo XXI".
+
+select titulo, nombre as editorial
+from libros l
+right join editoriales e
+on l.codigoeditorial = e.codigo
+where l.codigoeditorial is not null;
+
+-- Mostramos las editoriales que NO están presentes en "libros" (que NO encuentran coincidencia en "editoriales"):
+-- Solamente aparece la editorial "Siglo XXI".
+
+select nombre
+from libros l
+right join editoriales e
+on l.codigoeditorial = e.codigo
+where l.codigoeditorial is null;
 
 
 
