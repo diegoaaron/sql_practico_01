ebb0ce41d02547f79465641650322f68cd8a4c52
Diego Aarón <diegoaarondv@gmail.com>
2022-09-15 21:40:53 -0500

    combinacion cruzada con CROSS JOIN

--- 052_cross_join_combinacion_cruzada.sql
+++ 052_cross_join_combinacion_cruzada.sql
@@ -102,10 +102,62 @@ insert into varones values('Juan Garcia','Peru 333',50);
  
  -- Realice la misma combinación pero considerando solamente las personas mayores de 40 años (6 filas)
  
-  select m.nombre, v.nombre 
+select m.nombre, v.nombre 
  from mujeres m
  cross join varones v
  where m.edad > 40 and v.edad > 40;
  
- -- 
+ -- Forme las parejas pero teniendo en cuenta que no tengan una diferencia superior a 10 años (8 filas)
  
+select m.nombre, v.nombre 
+ from mujeres m
+ cross join varones v
+ where m.edad -  v.edad  between -10 and 10;
+ 
+-- Ejercicio 2 
+
+drop table guardias;
+drop table tareas;
+
+create table guardias(
+  documento char(8),
+  nombre varchar2(30),
+  sexo char(1), /* 'f' o 'm' */
+  domicilio varchar2(30),
+  primary key (documento)
+);
+
+create table tareas(
+  codigo number(2),
+  domicilio varchar2(30),
+  descripcion varchar2(30),
+  horario char(2), /* 'AM' o 'PM'*/
+  primary key (codigo)
+);
+
+insert into guardias values('22333444','Juan Perez','m','Colon 123');
+insert into guardias values('24333444','Alberto Torres','m','San Martin 567');
+insert into guardias values('25333444','Luis Ferreyra','m','Chacabuco 235');
+insert into guardias values('23333444','Lorena Viale','f','Sarmiento 988');
+insert into guardias values('26333444','Irma Gonzalez','f','Mariano Moreno 111');
+
+insert into tareas values(1,'Colon 1111','vigilancia exterior','AM');
+insert into tareas values(2,'Urquiza 234','vigilancia exterior','PM');
+insert into tareas values(3,'Peru 345','vigilancia interior','AM');
+insert into tareas values(4,'Avellaneda 890','vigilancia interior','PM');
+ 
+-- La empresa quiere que todos sus empleados realicen todas las tareas. Realice un "cross join" (20 filas)
+
+select g.nombre, t.descripcion
+from guardias g
+cross join tareas t;
+
+
+-- En este caso, la empresa quiere que todos los guardias de sexo femenino realicen las tareas de "vigilancia interior" 
+-- y los de sexo masculino de "vigilancia exterior". Realice una "cross join" con un "where" que controle tal requisito (10 filas)
+
+select g.nombre, t.descripcion
+from guardias g
+cross join tareas t
+where (g.sexo = 'f' and t.descripcion = 'vigilancia interior') or (g.sexo='m' and t.descripcion='vigilancia exterior');
+
