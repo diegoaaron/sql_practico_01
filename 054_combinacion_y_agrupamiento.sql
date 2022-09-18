8be8f5f945c2497725fe1b422a038ad60f8ed67b
Diego Aarón <diegoaarondv@gmail.com>
2022-09-17 22:07:14 -0500

    combinacion y agrupamiento de tablas

--- 054_combinacion_y_agrupamiento.sql
+++ 054_combinacion_y_agrupamiento.sql
@@ -73,7 +73,39 @@ left join libros l
 on codigoeditorial = e.codigo
 group by e.nombre;
 
+-- Ejercicio 1
 
+ drop table visitantes;
+ drop table ciudades;
+
+ create table visitantes(
+  nombre varchar2(30),
+  edad number(2),
+  sexo char(1) default 'f',
+  domicilio varchar2(30),
+  codigociudad number(2),
+  mail varchar(30),
+  montocompra decimal (6,2)
+ );
+
+ create table ciudades(
+  codigo number(2),
+  nombre varchar(20)
+ );
+ 
+
+ insert into ciudades values(1,'Cordoba');
+ insert into ciudades values(2,'Carlos Paz');
+ insert into ciudades values(3,'La Falda');
+ insert into ciudades values(4,'Cruz del Eje');
+
+ insert into visitantes values ('Susana Molina', 35,'f','Colon 123', 1, null,59.80);
+ insert into visitantes values ('Marcos Torres', 29,'m','Sucre 56', 1, 'marcostorres@hotmail.com',150.50);
+ insert into visitantes values ('Mariana Juarez', 45,'f','San Martin 111',2,null,23.90);
+ insert into visitantes values ('Fabian Perez',36,'m','Avellaneda 213',3,'fabianperez@xaxamail.com',0);
+ insert into visitantes values ('Alejandra Garcia',28,'f',null,2,null,280.50);
+ insert into visitantes values ('Gaston Perez',29,'m',null,5,'gastonperez1@gmail.com',95.40);
+ insert into visitantes values ('Mariana Juarez',33,'f',null,2,null,90);
 
 
 
