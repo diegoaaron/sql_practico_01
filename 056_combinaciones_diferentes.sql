b4d85d59d600d5ab45159eeb85f10f3502a92e15
Diego Aarón <diegoaarondv@gmail.com>
2022-09-18 20:56:32 -0500

    combinaciones usando + USING NATURAL

--- 056_combinaciones_diferentes.sql
+++ 056_combinaciones_diferentes.sql
@@ -117,9 +117,80 @@ join editoriales e
 on l.codigoeditorial(+)= e.codigoeditorial(+);
 */
 
+ drop table libros;
+ drop table editoriales;
 
+ create table libros(
+  codigo number(5),
+  titulo varchar2(40),
+  autor varchar2(30),
+  codigoeditorial number(3)
+ );
+ create table editoriales(
+  codigoeditorial number(3),
+  nombre varchar2(20)
+);
 
+ insert into editoriales values(1,'Planeta');
+ insert into editoriales values(2,'Emece');
+ insert into editoriales values(3,'Siglo XXI');
+ insert into editoriales values(null,'Norma');
 
+ insert into libros values(100,'El aleph','Borges',1);
+ insert into libros values(101,'Martin Fierro','Jose Hernandez',1);
+ insert into libros values(102,'Aprenda PHP','Mario Molina',2);
+ insert into libros values(103,'Java en 10 minutos',null,4);
+ insert into libros values(104,'El anillo del hechicero','Carol Gaskin',null);
 
+-- Realizamos un natural join entre las dos tablas
+-- En el ejemplo anterior la tabla "libros" combina su campo "codigoeditorial" con el campo "codigoeditorial" de "editoriales".
 
+select titulo, nombre as editorial
+from libros
+natural join editoriales;
+
+-- Realizamos una combinación empleando la cláusula "using"
+-- En el ejemplo anterior la tabla "libros" combina su campo "codigoeditorial" con el campo "codigoeditorial" de "editoriales".
+
+select titulo, nombre as editorial
+from libros
+join editoriales 
+using (codigoeditorial);
+
+-- Realizamos una combinación izquierda y luego un "join" con el modificador "(+)"; ambas consultas retornan el mismo resultado
+-- Ambas mostrarán el título y nombre de la editorial; los libros cuyo código de editorial no esté presente en "editoriales" 
+-- aparecerán con el valor "null" en la columna "editorial".
+
+select titulo, nombre as editorial
+from libros l
+left join editoriales e
+on l.codigoeditorial = e.codigoeditorial;
+
+select titulo, nombre as editorial
+from libros l
+join editoriales e
+on l.codigoeditorial = e.codigoeditorial(+);
+
+-- Realizamos una combinación derecha y luego obtenemos el mismo resultado empleando "join" y el modificador "(+)"
+-- Ambas mostrarán el título y nombre de la editorial; las editoriales que no encuentran coincidencia en "libros", 
+-- aparecen con el valor "null" en la columna "titulo".
+
+select titulo, nombre as editorial
+from editoriales e
+right join libros l
+on e.codigoeditorial = l.codigoeditorial;
+
+select titulo, nombre as editorial
+from editoriales e
+join libros l
+on e.codigoeditorial(+) = l.codigoeditorial;
+
+-- Si intentamos emplear el modificador en campos de distintas tablas Oracle mostrará un mensaje de error:
+
+select titulo, nombre as editorial
+from libros l
+join editoriales e
+on l.codigoeditorial(+) = e.codigoeditorial(+);
+
+-- Ejercicio 1
 
