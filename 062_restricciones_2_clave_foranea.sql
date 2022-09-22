7f0f82a0b02ce66d48ef4001f1adde2e2ab79a10
Diego Aarón <diegoaarondv@gmail.com>
2022-09-21 19:57:43 -0500

    restricciones en llaves foraneas

--- 062_restricciones_2_clave_foranea.sql
+++ 062_restricciones_2_clave_foranea.sql
@@ -74,3 +74,79 @@ existe referenciada en la tabla que tiene dicha restricción, la acción no se eje
 sucede porque, por defecto (y como única opción), para actualizaciones existe "no action".
 */
 
+ drop table libros;
+ drop table editoriales;
+
+ create table libros(
+  codigo number(5),
+  titulo varchar2(40),
+  autor varchar2(30),
+  codigoeditorial number(3),
+  primary key (codigo)
+ );
+ create table editoriales(
+  codigo number(3),
+  nombre varchar2(20),
+  primary key (codigo)
+);
+
+ insert into editoriales values(1,'Emece');
+ insert into editoriales values(2,'Planeta');
+ insert into editoriales values(3,'Siglo XXI');
+
+ insert into libros values(1,'El aleph','Borges',1);
+ insert into libros values(2,'Martin Fierro','Jose Hernandez',2);
+ insert into libros values(3,'Aprenda PHP','Mario Molina',2);
+ insert into libros values(4,'El anillo del hechicero','Gaskin',3);
+
+-- Establecemos una restricción "foreign key" para evitar que se ingrese en "libros" un código de editorial inexistente en 
+-- "editoriales" con la opción "on cascade" para eliminaciones:
+
+alter table libros
+add constraint FK_LIBROS_CODIGOEDITORIAL
+foreign key (codigoeditorial)
+references editoriales(codigo)
+on delete cascade;
+
+-- Consultamos "user_constraints":
+-- En la columna "delete_rule" de la restricción "foreign key" mostrará "cascade".
+
+select constraint_name, constraint_type, delete_rule
+from user_constraints
+where table_name = 'LIBROS';
+
+-- Si eliminamos una editorial, se borra tal editorial de "editoriales" y todos los registros de "libros" de dicha editorial:
+
+delete from editoriales where codigo = 1; 
+
+-- Veamos si la eliminación se extendió a "libros":
+-- El libro "El aleph", de la editorial con código 1 se ha eliminado.
+
+select * from libros;
+
+-- Eliminamos la restricción "foreign key" de "libros":
+
+alter table libros
+drop constraint FK_LIBROS_CODIGOEDITORIAL;
+
+-- Establecemos una restricción "foreign key" sobre "codigoeditorial" de "libros" con la opción "set null" para eliminaciones:
+
+alter table libros
+add constraint FK_LIBROS_CODIGOEDITORIAL
+foreign key (codigoeditorial)
+references editoriales(codigo)
+on delete set null;
+
+-- Consultamos "user_constraints":
+-- En la columna "delete_rule" de la restricción "foreign key" mostrará "set null".
+
+select constraint_name, constraint_type, delete_rule
+from user_constraints
+where table_name = 'LIBROS';
+
+-- Si eliminamos una editorial cuyo código está presente en "libros", se borra tal editorial de "editoriales" y todos los registros 
+-- de "libros" de dicha editorial se setean con el valor "null":
+
+
+
+
