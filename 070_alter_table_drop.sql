bbdc8ed276d97dd19fa5405a01ede66f7317a35a
Diego Aarón <diegoaarondv@gmail.com>
2022-09-25 23:26:00 -0500

    modificar tabla - eliminar campo

--- 070_alter_table_drop.sql
+++ 070_alter_table_drop.sql
@@ -109,3 +109,101 @@ drop column titulo;
 
 -- Ejercicio 1 
 
+ drop table empleados;
+ drop table secciones;
+
+ create table secciones(
+  codigo number(2),
+  nombre varchar(20),
+  primary key (codigo)
+ );
+
+ create table empleados(
+  apellido varchar2(20),
+  nombre varchar2(20) not null,
+  domicilio varchar2(30),
+  seccion number(2),
+  sueldo number(8,2),
+  constraint CK_empleados_sueldo
+   check (sueldo>=0) disable,
+  fechaingreso date,
+  constraint FK_empleados_seccion
+   foreign key (seccion)
+   references secciones(codigo)
+   on delete set null
+ );
+
+ insert into secciones values(8,'Secretaria');
+ insert into secciones values(9,'Contaduria');
+ insert into secciones values(10,'Sistemas');
+
+ insert into empleados values('Lopez','Juan','Colon 123',8,505.50,'10/10/1980');
+ insert into empleados values('Gonzalez','Juana','Avellaneda 222',9,600,'01/05/1990');
+ insert into empleados values('Perez','Luis','Caseros 987',10,800,'12/09/2000');
+ 
+-- Elimine el campo "domicilio" y luego verifique la eliminación
+
+alter table empleados
+drop column domicilio;
+
+describe empleados;
+
+-- Vea las restricciones de "empleados" (1 restricción "foreign key" y 2 "check")
+
+select * from user_constraints
+where table_name = 'EMPLEADOS';
+
+-- Intente eliminar el campo "codigo" de "secciones"
+
+alter table secciones
+drop column codigo;
+
+-- Elimine la restricción "foreign key" de "empleados", luego elimine el campo "codigo" de "secciones" y verifique la eliminación
+
+alter table empleados
+drop constraint FK_EMPLEADOS_SECCION;
+
+alter table secciones
+drop column codigo;
+
+-- Verifique que al eliminar el campo "codigo" de "secciones" se ha eliminado la "primary key" de "secciones"
+
+describe secciones;
+
+-- Elimine el campo "sueldo" y verifique que la restricción sobre tal campo se ha eliminado
+
+alter table empleados
+drop column sueldo;
+
+select * from user_constraints
+where table_name = 'EMPLEADOS';
+
+-- Cree un índice no único por el campo "apellido" y verifique su existencia consultando "user_indexes"
+
+create index I_EMPLEADOS_APELLIDO
+on empleados(apellido);
+
+
+select * from user_indexes
+where table_name = 'EMPLEADOS';
+
+-- Elimine el campo "apellido" y verifique que el índice se ha eliminado
+
+alter table empleados
+drop column apellido;
+
+-- Elimine 2 campos de "empleados" y vea la estructura de la tabla
+
+alter table empleados
+drop column seccion;
+
+alter table empleados
+drop column fechaingreso;
+
+-- Intente eliminar el único campo de "empleados"
+ 
+ describe empleados;
+ 
+ alter table empleados
+drop column nombre;
+ 
