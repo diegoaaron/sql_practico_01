325527d0834fead3f07165b23e547cd52ce4c4d7
Diego Aarón <diegoaarondv@gmail.com>
2022-09-27 17:08:18 -0500

    agregando campos y restriccciones a las tablas

--- 073_subconsultas_con_expresiones.sql
+++ 073_subconsultas_con_expresiones.sql
@@ -85,4 +85,51 @@ where precio = (select max(precio) from libros);
 delete from libros
 where precio = (select min(precio) from libros);
 
+-- Ejercicio 1
+
+ drop table alumnos;
+ 
+  create table alumnos(
+  documento char(8),
+  nombre varchar2(30),
+  nota number(4,2),
+  primary key(documento),
+  constraint CK_alumnos_nota_valores check (nota>=0 and nota <=10)
+ );
+
+ insert into alumnos values('30111111','Ana Algarbe',5.1);
+ insert into alumnos values('30222222','Bernardo Bustamante',3.2);
+ insert into alumnos values('30333333','Carolina Conte',4.5);
+ insert into alumnos values('30444444','Diana Dominguez',9.7);
+ insert into alumnos values('30555555','Fabian Fuentes',8.5);
+ insert into alumnos values('30666666','Gaston Gonzalez',9.70);
+
+-- Obtenga todos los datos de los alumnos con la nota más alta, empleando subconsulta
+
+select * from alumnos
+where nota = (select max(nota) from alumnos);
+
+-- Realice la misma consulta anterior pero intente que la consulta interna retorne, además del máximo valor de nota, el 
+-- nombre del alumno. Mensaje de error, porque la lista de selección de una subconsulta que va luego de un operador de 
+-- comparación puede incluir sólo un campo o expresión (excepto si se emplea "exists" o "in").
+
+select * from alumnos
+where nota = (select nombre, max(nota) from alumnos);
+
+-- Muestre los alumnos que tienen una nota menor al promedio, su nota, y la diferencia con el promedio.
+
+select nombre, nota, nota - (select avg(nota) from alumnos) as diferencia 
+from alumnos
+where nota < (select avg(nota) from alumnos);
+
+-- Cambie la nota del alumno que tiene la menor nota por 4.
+
+update alumnos set nota = 4
+where nota = (select min(nota) from alumnos);
+
+-- Elimine los alumnos cuya nota es menor al promedio.
+
+delete from alumnos
+where nota < (select avg(nota) from alumnos);
+
 
