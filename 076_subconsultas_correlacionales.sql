2f7f10ec6b06016cc3762845a6243e82a87154b0
Diego Aarón <diegoaarondv@gmail.com>
2022-09-27 22:11:22 -0500

    subconsultas correlacionales

--- 076_subconsultas_correlacionales.sql
+++ 076_subconsultas_correlacionales.sql
@@ -76,3 +76,73 @@ from facturas f;
 
 -- Ejercicio 1
 
+ drop table inscriptos;
+ drop table socios;
+
+ create table socios(
+  numero number(4),
+  documento char(8),
+  nombre varchar2(30),
+  domicilio varchar2(30),
+  primary key (numero)
+ );
+ 
+ create table inscriptos (
+  numerosocio number(4) not null,
+  deporte varchar(20) not null,
+  cuotas number(2) default 0,
+  constraint CK_inscriptos_cuotas
+   check (cuotas>=0 and cuotas<=10),
+  primary key(numerosocio,deporte),
+  constraint FK_inscriptos_socio
+   foreign key (numerosocio)
+   references socios(numero)
+   on delete cascade
+ );
+
+ insert into socios values(1,'23333333','Alberto Paredes','Colon 111');
+ insert into socios values(2,'24444444','Carlos Conte','Sarmiento 755');
+ insert into socios values(3,'25555555','Fabian Fuentes','Caseros 987');
+ insert into socios values(4,'26666666','Hector Lopez','Sucre 344');
+
+ insert into inscriptos values(1,'tenis',1);
+ insert into inscriptos values(1,'basquet',2);
+ insert into inscriptos values(1,'natacion',1);
+ insert into inscriptos values(2,'tenis',9);
+ insert into inscriptos values(2,'natacion',1);
+ insert into inscriptos values(2,'basquet',default);
+ insert into inscriptos values(2,'futbol',2);
+ insert into inscriptos values(3,'tenis',8);
+ insert into inscriptos values(3,'basquet',9);
+ insert into inscriptos values(3,'natacion',0);
+ insert into inscriptos values(4,'basquet',10);
+ 
+-- Se necesita un listado de todos los socios que incluya nombre y domicilio, la cantidad de deportes a los cuales se 
+-- ha inscripto, empleando subconsulta.
+
+select s.*, 
+(select count(i.deporte) from inscriptos i
+where s.numero = i.numerosocio)  as "deportes inscriptos"
+from socios s;
+
+-- Se necesita el nombre de todos los socios, el total de cuotas que debe pagar (10 por cada deporte) y el total de cuotas 
+-- pagas, empleando subconsulta.
+
+select s.*, 
+(select count(i.deporte) * 10 from inscriptos i
+where s.numero = i.numerosocio)  as "cuotas a pagar",
+(select sum(i.cuotas) from inscriptos i
+where s.numero = i.numerosocio)  as "cuotas pagadas"
+from socios s;
+
+
+-- Obtenga la misma salida anterior empleando join.
+
+select s.nombre, 
+count(i.deporte) * 10 as "cuotas a pagar",
+sum(i.cuotas) as "cuotas pagadas"
+from socios s
+inner join inscriptos i
+on s.numero = i.numerosocio
+group by s.nombre;
+
