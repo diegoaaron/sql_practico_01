a494b76fc6620c04888cab8b071f676533f63c70
Diego Aarón <diegoaarondv@gmail.com>
2022-09-28 19:51:11 -0500

    filtrar registros con EXISTS

--- 077_exists.sql
+++ 077_exists.sql
@@ -93,3 +93,49 @@ where not exists
 (select * from detalles d
 where f.numero =d.numerofactura
 and d.articulo='lapiz');
+
+-- Ejercicio 1 
+
+ drop table inscriptos;
+ drop table socios;
+
+ create table socios(
+  numero number(4),
+  documento char(8),
+  nombre varchar2(30),
+  domicilio varchar2(30),
+  primary key (numero)
+ );
+ 
+ create table inscriptos (
+  numerosocio number(4) not null,
+  deporte varchar2(20) not null,
+  cuotas number(2) default 0,
+  constraint CK_inscriptos_cuotas
+   check (cuotas>=0 and cuotas<=10),
+  primary key(numerosocio,deporte),
+  constraint FK_inscriptos_socio
+   foreign key (numerosocio)
+   references socios(numero)
+   on delete cascade
+ );
+
+ insert into socios values(1,'23333333','Alberto Paredes','Colon 111');
+ insert into socios values(2,'24444444','Carlos Conte','Sarmiento 755');
+ insert into socios values(3,'25555555','Fabian Fuentes','Caseros 987');
+ insert into socios values(4,'26666666','Hector Lopez','Sucre 344');
+
+ insert into inscriptos values(1,'tenis',1);
+ insert into inscriptos values(1,'basquet',2);
+ insert into inscriptos values(1,'natacion',1);
+ insert into inscriptos values(2,'tenis',9);
+ insert into inscriptos values(2,'natacion',1);
+ insert into inscriptos values(2,'basquet',default);
+ insert into inscriptos values(2,'futbol',2);
+ insert into inscriptos values(3,'tenis',8);
+ insert into inscriptos values(3,'basquet',9);
+ insert into inscriptos values(3,'natacion',0);
+ insert into inscriptos values(4,'basquet',10);
+
+
+
