c02ac252c0836f639500dfb56c1ef8570dd2a79c
Diego Aarón <diegoaarondv@gmail.com>
2022-10-01 18:58:42 -0500

    INSERT a través de subconsultas

--- 080_subconsultas_insert.sql
+++ 080_subconsultas_insert.sql
@@ -27,4 +27,27 @@ La cantidad de columnas devueltas en la consulta debe ser la misma que la cantid
 Se pueden insertar valores en una tabla con el resultado de una consulta que incluya cualquier tipo de "join".
 */
 
+ drop table alumnos;
+ drop table aprobados;
+
+ create table alumnos(
+  documento char(8) not null,
+  nombre varchar2(30),
+  nota number(4,2)
+   constraint CK_alumnos_nota_valores check (nota>=0 and nota <=10),
+  primary key(documento)
+ );
+
+ create table aprobados(
+  documento char(8) not null,
+  nota number(4,2)
+   constraint CK_aprobados_nota_valores check (nota>=0 and nota <=10),
+  primary key(documento)
+ );
+
+ insert into alumnos values('30000000','Ana Acosta',8);
+ insert into alumnos values('30111111','Betina Bustos',9);
+ insert into alumnos values('30222222','Carlos Caseros',2.5); 
+ insert into alumnos values('30333333','Daniel Duarte',7.7);
+ insert into alumnos values('30444444','Estela Esper',3.4);
 
