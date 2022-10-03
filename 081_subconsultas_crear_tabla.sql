666e63231eb49ba2f6ed7c2215ba29ea65b360a3
Diego Aarón <diegoaarondv@gmail.com>
2022-10-02 22:24:20 -0500

    crear tablas utilizando subqueries

--- 081_subconsultas_crear_tabla.sql
+++ 081_subconsultas_crear_tabla.sql
@@ -24,3 +24,48 @@ la tabla consultada; si se quiere dar otro nombre a los campos de la nueva tabla
 Podemos emplear "group by", funciones de agrupamiento y "order by" en las consultas. También podemos crear 
 una tabla que contenga datos de 2 o más tablas empleando combinaciones.
 */
+
+ drop table libros;
+ drop table editoriales;
+
+ create table libros( 
+  codigo number(5),
+  titulo varchar2(40) not null,
+  autor varchar2(30),
+  editorial varchar2(20),
+  precio number(5,2),
+  primary key(codigo)
+ ); 
+
+ insert into libros values(1,'Uno','Richard Bach','Planeta',15);
+ insert into libros values(2,'El aleph','Borges','Emece',25);
+ insert into libros values(3,'Matematica estas ahi','Paenza','Nuevo siglo',18);
+ insert into libros values(4,'Aprenda PHP','Mario Molina','Nuevo siglo',45);
+ insert into libros values(5,'Ilusiones','Richard Bach','Planeta',14);
+ insert into libros values(6,'Java en 10 minutos','Mario Molina','Nuevo siglo',50);
+ 
+ -- Creamos una tabla llamada "editoriales" que contenga los nombres de las editoriales obteniendo tales nombres de la 
+ -- tabla "libros":
+
+create table editoriales as 
+(select distinct editorial as nombre
+from libros);
+
+ -- Veamos la nueva tabla:
+
+select * from editoriales;
+ 
+ -- Necesitamos una nueva tabla llamada "librosporeditorial" que contenga la cantidad de libros de cada editorial. Primero 
+ -- eliminamos la tabla:
+
+drop table cantidadporeditorial;
+ 
+ -- Creamos la nueva tabla empleando una subconsulta:
+
+create table cantidadporeditorial as
+(select editorial as nombre, count(*) as cantidad
+from libros
+group by editorial);
+ 
+ -- Veamos los registros de la nueva tabla:
+
