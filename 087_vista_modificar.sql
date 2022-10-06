af28623a1085e4406499589e07b11fd33e661c7a
Diego Aarón <diegoaarondv@gmail.com>
2022-10-05 23:23:27 -0500

    modificar con CREATE o REPLACE

---
+++ 087_vista_modificar.sql
@@ -0,0 +1,11 @@
+/*
+Para modificar una vista puede eliminarla y volver a crearla o emplear "create or replace".
+
+Sintaxis:
+
+ create or replace view NOMBREVISTA
+  as SUBCONSULTA;
+  
+Con "create or replace view" se modifica la definición de una vista existente o se crea si no existe.
+*/
+
