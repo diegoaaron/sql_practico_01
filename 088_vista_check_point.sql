3eaef9c02b17b5df768d0728b4c2fb3605a00761
Diego Aarón <diegoaarondv@gmail.com>
2022-10-06 10:26:19 -0500

    vistas con CHECK POINT activado

--- 088_vista_check_point.sql
+++ 088_vista_check_point.sql
@@ -24,3 +24,39 @@ Sintaxis básica:
   as SUBCONSULTA
   with check option;
 */
+
+ drop table empleados;
+
+ create table empleados(
+  documento char(8),
+  sexo char(1)
+   constraint CK_empleados_sexo check (sexo in ('f','m')),
+  apellido varchar2(20),
+  nombre varchar2(20),
+  seccion varchar2(30),
+  cantidadhijos number(2),
+  constraint CK_empleados_hijos check (cantidadhijos>=0),
+  estadocivil char(10)
+  constraint CK_empleados_estadocivil check (estadocivil in ('casado','divorciado','soltero','viudo'))
+);
+
+ insert into empleados values('22222222','f','Lopez','Ana','Administracion',2,'casado');
+ insert into empleados values('23333333','m','Lopez','Luis','Administracion',0,'soltero');
+ insert into empleados values('24444444','m','Garcia','Marcos','Sistemas',3,'divorciado');
+ insert into empleados values('25555555','m','Gomez','Pablo','Sistemas',2,'casado');
+ insert into empleados values('26666666','f','Perez','Laura','Contaduria',3,'casado');
+
+-- Creamos o reemplazamos (si existe) la vista "vista_empleados", para que muestre el nombre, apellido, sexo y sección de todos los empleados de 
+-- "Administracion" agregando la cláusula "with check option" para evitar que se modifique la sección de tales empleados a través de la vista y que 
+-- se ingresen empleados de otra sección:
+
+create or replace view vista_empleados as 
+select apellido, nombre, sexo, seccion from empleados 
+where seccion = 'Administracion' 
+with check option;
+
+-- Consultamos la vista:
+
+select * from vista_empleados;
+
+-- 
