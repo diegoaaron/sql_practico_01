1f353a564f0c25efbfd2db63eb6796525f3f1342
Diego Aarón <diegoaarondv@gmail.com>
2022-10-05 23:29:14 -0500

    modificar con CREATE o REPLACE

--- 087_vista_modificar.sql
+++ 087_vista_modificar.sql
@@ -9,3 +9,44 @@ Sintaxis:
 Con "create or replace view" se modifica la definición de una vista existente o se crea si no existe.
 */
 
+ drop table empleados;
+ drop table secciones;
+
+ create table secciones(
+  codigo number(2),
+  nombre varchar2(20),
+  constraint PK_secciones primary key (codigo)
+ );
+
+ create table empleados(
+  documento char(8),
+  nombre varchar2(30),
+  domicilio varchar2(30),
+  seccion number(2) not null,
+  constraint FK_empleados_seccion
+   foreign key (seccion)
+   references secciones(codigo),
+  constraint PK_empleados
+   primary key (documento)
+);
+
+ insert into secciones values(1,'Administracion');
+ insert into secciones values(2,'Contaduría');
+ insert into secciones values(3,'Sistemas');
+
+ insert into empleados values('22222222','Lopez Ana','Colon 123',1);
+ insert into empleados values('23333333','Lopez Luis','Sucre 235',1);
+ insert into empleados values('24444444','Garcia Marcos','Sarmiento 1234',2);
+ insert into empleados values('25555555','Gomez Pablo','Bulnes 321',3);
+ insert into empleados values('26666666','Perez Laura','Peru 1254',3);
+ 
+ -- Eliminamos la vista "vista_empleados":
+
+  drop view vista_empleados;
+
+-- Creamos la vista "vista_empleados" que muestre algunos campos de los empleados de la sección 1:
+
+create view vista_empleados as 
+select documento, nombre, seccion from empleados
+where seccion = 1;
+
