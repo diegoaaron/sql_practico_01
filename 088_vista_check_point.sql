80f1657f2551cba550bf92f52865f2f913f63625
Diego Aarón <diegoaarondv@gmail.com>
2022-10-06 10:34:57 -0500

    vistas con CHECK POINT activado

--- 088_vista_check_point.sql
+++ 088_vista_check_point.sql
@@ -87,3 +87,78 @@ insert into vista_empleados values ('Torres', 'Tatiana', 'f', 'Sistemas');
 
 -- Ejercicio 1 
 
+ drop table clientes;
+
+ create table clientes(
+  nombre varchar2(40),
+  documento char(8),
+  domicilio varchar2(30),
+  ciudad varchar2(30)
+ );
+
+ insert into clientes values('Juan Perez','22222222','Colon 1123','Cordoba');
+ insert into clientes values('Karina Lopez','23333333','San Martin 254','Cordoba');
+ insert into clientes values('Luis Garcia','24444444','Caseros 345','Cordoba');
+ insert into clientes values('Marcos Gonzalez','25555555','Sucre 458','Santa Fe');
+ insert into clientes values('Nora Torres','26666666','Bulnes 567','Santa Fe');
+ insert into clientes values('Oscar Luque','27777777','San Martin 786','Santa Fe');
+ insert into clientes values('Pedro Perez','28888888','Colon 234','Buenos Aires');
+ insert into clientes values('Rosa Rodriguez','29999999','Avellaneda 23','Buenos Aires');
+ 
+ -- Cree o reemplace la vista "vista_clientes" para que recupere el nombre y ciudad de todos los clientes que no sean de "Cordoba" sin emplear 
+ -- "with check option"
+
+ create or replace view vista_clientes
+ as
+  select nombre, ciudad
+  from clientes
+  where ciudad <>'Cordoba';
+
+ create or replace view vista_clientes2
+ as
+  select nombre, ciudad
+  from clientes
+  where ciudad <>'Cordoba'
+  with check option;
+
+ select *from vista_clientes;
+ select *from vista_clientes2;
+
+ update vista_clientes2 set ciudad='Cordoba' where nombre='Pedro Perez';
+
+ update vista_clientes set ciudad='Cordoba' where nombre='Pedro Perez';
+
+ update vista_clientes2 set ciudad='Buenos Aires' where nombre='Oscar Luque';
+
+ select *from vista_clientes2;
+
+ insert into vista_clientes2 values('Viviana Valdez','Cordoba');
+
+ insert into vista_clientes values('Viviana Valdez','Cordoba');
+
+ insert into vista_clientes2 values('Viviana Valdez','Salta');
+
+ select *from vista_clientes2;
+
+-- Cree o reemplace la vista "vista_clientes2" para que recupere el nombre y ciudad de todos los clientes que no sean de "Cordoba" empleando 
+-- "with check option"
+
+-- Consulte ambas vistas
+
+-- Intente modificar la ciudad del cliente "Pedro Perez" a "Cordoba" través de la vista que está restringida.
+
+-- Realice la misma modificación que intentó en el punto anterior a través de la vista que no está restringida
+
+-- Actualice la ciudad del cliente "Oscar Luque" a "Buenos Aires" mediante la vista restringida
+
+-- Verifique que "Oscar Luque" aún se incluye en la vista
+
+-- Intente ingresar un empleado de "Cordoba" en la vista restringida
+
+-- Ingrese el empleado anterior a través de la vista no restringida
+
+-- Ingrese un empleado de "Salta" en la vista restringida
+
+-- Verifique que el nuevo registro está incluido en la vista
+ 
+ 
