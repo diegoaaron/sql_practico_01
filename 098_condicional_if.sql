1085fab76039902488ab3849ef6e1cdffebeb81d
Diego Aarón <diegoaarondv@gmail.com>
2022-10-10 20:49:15 -0500

    condicional if

--- 098_condicional_if.sql
+++ 098_condicional_if.sql
@@ -18,4 +18,23 @@ Sintaxis:
    SENTENCIAS-- si la condición resulta falsa
  end if; 
 */
+ drop table notas;
+
+ create table notas(
+  nombre varchar2(30),
+  nota number(4,2)
+ );
+
+ insert into notas values('Acosta Ana', 6.7);
+ insert into notas values('Bustos Brenda', 9.5);
+ insert into notas values('Caseros Carlos', 3.7);
+ insert into notas values('Dominguez Daniel', 2);
+ insert into notas values('Fuentes Federico', 8);
+ insert into notas values('Gonzalez Gaston', 7);
+ insert into notas values('Juarez Juana', 4);
+ insert into notas values('Lopez Luisa',5.3);
+
+-- Creamos o reemplazamos la función "f_condicion" que recibe una nota y retorna una cadena de caracteres indicando 
+-- si aprueba o no:
+
 
