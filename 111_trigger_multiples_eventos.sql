efe8b34d99d81d85335b4209e975709c20f9acc7
Diego Aarón <diegoaarondv@gmail.com>
2022-10-16 21:22:41 -0500

    disparador de multiples eventos

--- 111_trigger_multiples_eventos.sql
+++ 111_trigger_multiples_eventos.sql
@@ -62,7 +62,95 @@ Las siguientes sentencias disparan el trigger creado anteriormente:
  delete from TABLA...;
 */
 
+ drop table libros;
+ drop table control;
 
+ create table libros(
+  codigo number(6),
+  titulo varchar2(40),
+  autor varchar2(30),
+  editorial varchar2(20),
+  precio number(6,2)
+ );
 
+ create table control(
+  usuario varchar2(30),
+  fecha date,
+  operacion varchar2(20)
+ );
 
+ insert into libros values(100,'Uno','Richard Bach','Planeta',25);
+ insert into libros values(103,'El aleph','Borges','Emece',28);
+ insert into libros values(105,'Matematica estas ahi','Paenza','Nuevo siglo',12);
+ insert into libros values(120,'Aprenda PHP','Molina Mario','Nuevo siglo',55);
+ insert into libros values(145,'Alicia en el pais de las maravillas','Carroll','Planeta',35);
+ 
+-- Establecemos el formato de fecha para que muestre "DD/MM/YYYY HH24:MI":
+
+alter session set NLS_DATE_FORMAT = 'DD/MM/YYYY HH24:MI';
+
+-- Creamos un disparador a nivel de sentencia, que se dispare cada vez que se ingrese, actualice o elimine un registro de 
+-- la tabla "libros". El trigger ingresa en la tabla "control", el nombre del usuario, la fecha y la hora en la cual se realizó la 
+-- modificación y el tipo de operación que se realizó:
+-- si se realizó una inserción (insert), se almacena "inserción";
+-- si se realizó una actualización (update), se almacena "actualización" y
+-- si se realizó una eliminación (delete) se almacena "borrado".
+
+create or replace trigger tr_cambio_libros
+before insert or update or delete on libros
+for each row
+begin
+if inserting then
+insert into control values (user, sysdate, 'insercion');
+end if;
+if updating then
+insert into control values (user, sysdate, 'actualizacion');
+end if;
+if deleting then
+insert into control values(user, sysdate, 'borrado');
+end if;
+end tr_cambio_libros;
+/
+
+-- Veamos qué nos informa el diccionario "user_triggers" respecto del trigger anteriormente creado:
+
+select * from user_triggers where trigger_name = 'TR_CAMBIO_LIBROS';
+
+-- obtenemos la siguiente información:
+-- trigger_name: nombre del disparador;
+-- trigger_type: momento y nivel, en este caso es un desencadenador "before" y a nivel de fila (each row);
+-- triggering_event: evento que lo dispara, en este caso, "insert or update or delete";
+-- base_object_type: a qué objeto está asociado, puede ser una tabla o una vista, en este caso, una tabla (table);
+-- table_name: nombre de la tabla al que está asociado (libros);
+
+-- Ingresamos un registro en "libros":
+
+insert into libros values(150, 'El experto en laberintos', 'Gaskin', 'Planeta', 23);
+
+-- Veamos si el trigger se disparó consultando la tabla "control":
+
+select * from control;
+
+-- Vemos que se ingresó un registro que muestra que el usuario "system", el día y hora actual realizó una inserción sobre "libros".
+
+-- Actualizamos algunos registros de "libros":
+
+update libros set precio = precio*1.1 where editorial = 'Planeta';
+
+ -- Veamos si el trigger se disparó consultando la tabla "control":
+
+select * from control;
+
+-- Vemos que se ingresaron 3 nuevos registros que muestran que el usuario "system", el día y hora actual actualizó tres 
+-- registros de "libros". Si el trigger se hubiese definido a nivel de sentencia, el "update" anterior se hubiese disparado 
+-- una sola vez.
+
+-- Eliminamos un registro de "libros":
+
+delete from libros where codigo = 145;
+
+-- Veamos si el trigger se disparó consultando la tabla "control":
+-- Vemos que se eliminó 1 registro.
+
+select * from control;
 
