ff3815582c8397ccd182e41ea5ab00758c0eef14
Diego Aarón <diegoaarondv@gmail.com>
2022-10-17 21:02:28 -0500

    eliminar trigger

---
+++ 116_trigger_delete.sql
@@ -0,0 +1,9 @@
+/*
+Para eliminar un trigger se emplea la siguiente sentencia:
+
+ drop trigger NOMBRETRIGGER;
+Ejemplo:
+
+ drop trigger tr_insertar_libros;
+Si eliminamos una tabla, se eliminan todos los triggers establecidos sobre ella.
+*/
